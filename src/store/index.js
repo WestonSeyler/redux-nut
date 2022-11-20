353e31fc2ef78c623a16912ff897b3aea22074fd
wujianing <jianing.wu@wowjoy.cn>
2022-11-20 23:12:19 +0800

    init redux

--- src/store/index.js
+++ src/store/index.js
@@ -1,14 +1,14 @@
-import { createStore, applyMiddleware, combineReducers } from "redux";
+import { createStore } from "../Redux-nut/index";
 // import { createStore, applyMiddleware, combineReducers } from "../redux-nut";
 
-import thunk from "redux-thunk";
-import promise from "redux-promise";
+// import thunk from "redux-thunk";
+// import promise from "redux-promise";
 
 // 定义了store修改规则
 export function countReducer(state = 0, action) {
   switch (action.type) {
     case "ADD":
-      return state + 1;
+      return state + 1;  
     case "MINUS":
       return state - 1;
     default:
@@ -18,15 +18,15 @@ export function countReducer(state = 0, action) {
 
 // 创建store
 const store = createStore(
-  // countReducer,
-  combineReducers({
-    count: countReducer,
-  }),
-  applyMiddleware(
-    promise,
-    thunk
-    //  logger
-  )
+  countReducer,
+  // combineReducers({
+  //   count: countReducer,
+  // }),
+  // applyMiddleware(
+  //   promise,
+  //   thunk
+  //   //  logger
+  // )
 );
 
 export default store;
