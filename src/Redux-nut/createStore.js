ad34f6597a709844a0261a5892ad600207fec441
wujianing <jianing.wu@wowjoy.cn>
2022-11-20 23:12:31 +0800

    feat:init redux

--- src/Redux-nut/createStore.js
+++ src/Redux-nut/createStore.js
@@ -1,5 +1,25 @@
-
-
-export default function createStore(){
-
+export default function createStore(reducer) {
+  let currentStore;
+  let currentListener = [];
+  function getState() {
+    return currentStore;
+  }
+  function dispatch(action) {
+    currentStore = reducer(currentStore, action);
+    currentListener.forEach((i) => i());
+  }
+  dispatch("ADDWDDD?DDDD");
+  function subscribe(listener) {
+    currentListener.push(listener);
+    return () => {
+      const index = currentListener.indexOf(listener);
+      currentListener.splice(index, 1);
+    };
+  }
+  return {
+    getState,
+    dispatch,
+    subscribe,
+    currentStore,
+  };
 }
